0862d94b347f26c2c864ea3168b1e9dc8cf8c633
suisha <gs9600@gmail.com>
2013-12-02 12:49:23 -0500

    renamed files

--- sixteen.rb
+++
@@ -1,6 +0,0 @@
-=begin
-215 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
-
-What is the sum of the digits of the number 2^1000?
-=end 
-puts (2**1000).to_s.chars.inject(0) { |s, i| s += i.to_i }
--- ten.rb
+++
@@ -1,6 +0,0 @@
-=begin
-The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
-
-Find the sum of all the primes below two million.
-=end 
-require 'prime'; puts Prime.each(2000000).to_a.inject(:+) 
